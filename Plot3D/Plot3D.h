d84cd9714203edbca9bc23c8a984437addfaf629
Shant Tokatyan <shant.tokatyan@dexcom.com>
2019-12-14 16:46:55 -0800

    first commit

---
+++ Plot3D/Plot3D.h
@@ -0,0 +1,19 @@
+//
+//  Plot3D.h
+//  Plot3D
+//
+//  Created by Shant Tokatyan on 12/14/19.
+//  Copyright © 2019 Stokaty. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Plot3D.
+FOUNDATION_EXPORT double Plot3DVersionNumber;
+
+//! Project version string for Plot3D.
+FOUNDATION_EXPORT const unsigned char Plot3DVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Plot3D/PublicHeader.h>
+
+
