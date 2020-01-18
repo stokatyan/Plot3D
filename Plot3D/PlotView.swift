6f4d2d87a0921f80c7b168d8fd158e8e2ccc25b8
Shant Tokatyan <shant.tokatyan@dexcom.com>
2020-01-18 08:51:53 -0800

    Added doc comments for PlotView.swift

--- Plot3D/PlotView.swift
+++ Plot3D/PlotView.swift
@@ -8,16 +8,25 @@
 
 import SceneKit
 
+/**
+ An object that manages the content of the `SCNView` that contains a plot in 3D.
+ */
 public class PlotView: UIView {
     
     // MARK: - Properties
     
+    /// The `SCNView` that contains all of the nodes being plotted.
     public private(set) var sceneView: SCNView
+    
+    /// The scene that contains all of the plotted elements.
     let scene: SCNScene
     
+    /// The node that contains everything being plotted.
     private let plotSpace: PlotSpaceNode
+    /// The camera node in the scene.
     public let cameraNode: SCNNode
     
+    /// The data source for the view's `plotSpaceNode`.
     public var dataSource: PlotDataSource? {
         get {
             return plotSpace.dataSource
@@ -27,6 +36,7 @@ public class PlotView: UIView {
         }
     }
     
+    /// The delegate for the view's `plotSpaceNode`.
     public var delegate: PlotDelegate? {
         get {
             return plotSpace.delegate
@@ -38,6 +48,9 @@ public class PlotView: UIView {
     
     // MARK: - Init
     
+    /**
+     Initializes a `PlotView` with the given frame.
+     */
     public override init(frame: CGRect) {
         sceneView = SCNView(frame: CGRect(x: 0, y: 0, width: frame.width, height: frame.height))
         scene = SCNScene()
@@ -45,11 +58,12 @@ public class PlotView: UIView {
         plotSpace = PlotSpaceNode(config: PlotConfiguration.defaultConfig)
         
         super.init(frame: frame)
-        plotSpace.plotView = self
-        addSubview(sceneView)
-        
         setupScene()
         setupCamera()
+        
+        addSubview(sceneView)
+        scene.rootNode.addChildNode(plotSpace)
+        plotSpace.plotView = self
         
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
         sceneView.addGestureRecognizer(tapGesture)
@@ -61,6 +75,10 @@ public class PlotView: UIView {
     
     // MARK: - Setup
     
+    /**
+     Sets up the camera in the scene.
+     The camera setup involves setting the possition of the camera, and what the camera is looking at.
+     */
     func setupCamera() {
         cameraNode.camera = SCNCamera()
         scene.rootNode.addChildNode(cameraNode)
@@ -69,17 +87,24 @@ public class PlotView: UIView {
         cameraNode.look(at: SCNVector3(x: 0, y: 0, z: 0))
     }
     
+    /**
+     Sets up the `sceneView`'s attributes.
+     */
     func setupScene() {
         sceneView.backgroundColor = .black
         sceneView.scene = scene
         sceneView.autoenablesDefaultLighting = true
         sceneView.allowsCameraControl = true
-        
-        scene.rootNode.addChildNode(plotSpace)
     }
     
     // MARK: - Actions
     
+    /**
+     Handles tha tapping of a plotted node.
+     
+     When a tap gesture is recognized, the scene is checked to see if a plotted node has been tapped.
+     If a plotted node is tapped, the action to be taken is delegated to the assigned `PlotDelegate`.
+     */
     @objc func handleTap(sender: UITapGestureRecognizer) {
         guard let delegate = delegate else {
             return
@@ -96,20 +121,37 @@ public class PlotView: UIView {
     
     // MARK: - Plotting
     
+    /**
+    Plots any points that were added aftter the latest call to `refresh` or `plotNewPoints`.
+    
+    This function is intended for cases where most of the data has already been plotted, and only a couple of additional points need to be plotted.
+    */
     public func plotNewPoints() {
         plotSpace.plotNewPoints()
     }
     
+    /**
+    Safely returns the node plotted at the given index.
+    - parameter index: the index of that plotted node.
+    - returns: an optional reference to the `SCNNode` that is plotted at the given index.
+    */
     public func plottedPoint(atIndex index: Int) -> SCNNode? {
         return plotSpace.plottedPoint(atIndex: index)
     }
     
+    /**
+    Reloads the plotted points using the plot data source and plot delegate.
+    This function is analagous to a `UITableView`'s `reloadData()` function.
+    */
     public func reloadData() {
         plotSpace.reloadData()
     }
         
     // MARK: - Getters
     
+    /**
+     - returns: An array containing all of the horizontal gridlines on the given plane.
+     */
     public func getHorizontalGridLines(_ plotPlane: PlotPlane) -> [SCNNode] {
         switch plotPlane {
         case .xy:
@@ -121,6 +163,9 @@ public class PlotView: UIView {
         }
     }
     
+    /**
+     - returns: An array containing all of the vertical gridlines on the given plane.
+     */
     public func getVerticalGridLines(_ plotPlane: PlotPlane) -> [SCNNode] {
         switch plotPlane {
         case .xy:
@@ -134,34 +179,66 @@ public class PlotView: UIView {
     
     // MARK: - Setters
     
+    /**
+     Sets the camera position using a `PlotPoint` instead of an SCNVector.
+     */
     public func setCamera(position: PlotPoint) {
         cameraNode.position = position.vector
     }
     
+    /**
+    Sets the camera orientation using a `PlotPoint` instead of an SCNVector.
+    */
     public func setCamera(lookAt position: PlotPoint) {
         cameraNode.look(at: position.vector)
     }
     
+    /**
+     Sets the `isHidden` property of all of the unit planes.
+     - parameter isHidden: Whether or not all of the unit planes should be hidden.
+     */
     public func setUnitPlanes(isHidden: Bool) {
         plotSpace.setUnitPlane(PlotPlane.xy, isHidden: isHidden)
         plotSpace.setUnitPlane(PlotPlane.xz, isHidden: isHidden)
         plotSpace.setUnitPlane(PlotPlane.yz, isHidden: isHidden)
     }
     
+    /**
+    Sets the `isHidden` property of the unit plane for the given plane.
+    - parameters:
+        - plotPlane: The plot plane that will be effected.
+        - isHidden: Whether or not the unit plane for the given plane should be hidden.
+    */
     public func setUnitPlan(_ plotPlane: PlotPlane, isHidden: Bool) {
         plotSpace.setUnitPlane(plotPlane, isHidden: isHidden)
     }
     
+    /**
+    Sets the `isHidden` property of the walls for each plane.
+    - parameter isHidden: Whether or not all of the walls should be hidden.
+    */
     public func setWalls(isHidden: Bool) {
         plotSpace.setWall(PlotPlane.xy, isHidden: isHidden)
         plotSpace.setWall(PlotPlane.xz, isHidden: isHidden)
         plotSpace.setWall(PlotPlane.yz, isHidden: isHidden)
     }
     
+    /**
+    Sets the `isHidden` property of the wall for the given plane.
+    - parameters:
+        - plotPlane: The plot plane that will be effected.
+        - isHidden: Whether or not wall for the given plane should be hidden.
+    */
     public func setWall(_ plotPlane: PlotPlane, isHidden: Bool) {
         plotSpace.setWall(plotPlane, isHidden: isHidden)
     }
     
+    /**
+    Sets the `color` property of the wall for the given plane.
+    - parameters:
+        - plotPlane: The plot plane that will be effected.
+        - color: The color for the wall on the given plane.
+    */
     public func setWall(_ plotPlane: PlotPlane, color: UIColor) {
         plotSpace.setWall(plotPlane, color: color)
     }
